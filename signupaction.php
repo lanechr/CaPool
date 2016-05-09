ab2278bc5d6184be5c7505bde3df34bdf9ccec4d
lanechr <lanechr@hotmail.com>
2016-05-09 11:39:26 +1000

    Sign up back end
    back end file for sign up field

---
+++ signupaction.php
@@ -0,0 +1,58 @@
+<?php 
+//Session Details
+session_start();
+$email = $_REQUEST['username'];
+$password = $_REQUEST['password'];
+
+if ($email == "" || $password == ""){
+    $_SESSION['signupfailednoinput'] = 1;
+    header('location:index.php');
+}else{
+    if (isset($_SESSION['signupfailednoinput'])) {
+        unset($_SESSION['signupfailednoinput']);
+	}
+
+    //Connect to database
+    $link = mysqli_connect("127.0.0.1", "root", "password", "capool");
+
+    // Check connection
+    if ($link->connect_error) {
+        die("CaPool table not found: " . $link->connect_error);
+    } 
+
+    // SQL Injection Protection
+    $email = stripslashes($email);
+    $password = stripslashes($password);
+    $email = mysqli_real_escape_string($link, $email);
+    $password = mysqli_real_escape_string($link, $password);
+
+    $sql="SELECT * FROM users WHERE email='$email'";
+
+    $result=mysqli_query($link, $sql);
+
+    if (mysqli_num_rows($result) == 1) {
+        //User Exists
+        $_SESSION['signupfailed'] = 1;
+        header('location:index.php');
+        echo "User Already Exists!";
+    } else {
+        
+        if (isset($_SESSION['signupfailed'])) {
+        unset($_SESSION['signupfailed']);
+	}
+        //User Doesn't Exist (Proceed)
+        // Query
+        $sql = "INSERT INTO users (email, password)
+        VALUES ('$email', '$password')";
+
+
+        if (mysqli_query($link, $sql)) {
+            echo "User entry created successfully<br>";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($link);
+        }
+    }
+
+    $link->close();
+}
+?>
