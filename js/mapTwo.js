57c532a5d3594e99b162e17ecf071dbe123afa53
lanechr <lanechr@hotmail.com>
2016-05-10 17:08:08 +1000

    removed alerts
    The removed alerts would pop up every 5 seconds if location was
    unavailable. We will need to think of a way to manage this.

--- js/mapTwo.js
+++ js/mapTwo.js
@@ -237,19 +237,19 @@ function errorFunc1(error) {
     switch(error.code) 
     {
     case error.PERMISSION_DENIED:
-      alert("User denied the request for Geolocation.");
+      //alert("User denied the request for Geolocation.");
       break;
     case error.POSITION_UNAVAILABLE:
-      alert("Location information is unavailable.")
+      //alert("Location information is unavailable.")
       break;
     case error.TIMEOUT:
-      alert("The request to get user location timed out.")
+      //alert("The request to get user location timed out.")
       break;
       case error.POSITION_ERROR:
-      alert("The request to get user location timed out.")
+      //alert("The request to get user location timed out.")
       break;
     case error.UNKNOWN_ERROR:
-      alert("An unknown error occurred.")
+      //alert("An unknown error occurred.")
       break;
     }
 }
