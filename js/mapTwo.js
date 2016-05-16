dffa349d1022adb7347d8f5c43853ec13ef250b9
lanechr <lanechr@hotmail.com>
2016-05-16 10:34:29 +1000

    Map starts centred on user location if possible

--- js/mapTwo.js
+++ js/mapTwo.js
@@ -175,6 +175,12 @@ function CenterControl(controlDiv, map) {
   });
 
 }
+        if (navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(getPositionForCentre, errorFunc);
+        }
+        else{
+            alert("Your browser does not support Location Services!")
+        };
 }
     
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
