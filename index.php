f0343e47e9f8b2e6069b2a734b947ae8a744afe3
lanechr <lanechr@hotmail.com>
2016-05-16 11:46:14 +1000

    Removing Facebook image
    Having to look into other ways of doing this as Azure security is not
    allowing it.

--- index.php
+++ index.php
@@ -57,13 +57,7 @@ session_start();
                 <div id="logo">
                     <a href="index.php"><h1>CaPool</h1></a>
                     <a href="index.php"><h2>Shared Travel, Simplified</h2></a>
-                    <?php 
-                        if (isset($_SESSION['FBID'])) {
-                            $pictureID = $_SESSION['FBID'];
-		                  echo "<div id='loggedInUserFBImg' style='background-image: url(\"http://graph.facebook.com/" . pictureID ."/picture?type=large\")'>" . pictureID ."</div>";
-                        }
-                    ?>
-                    <div id="loggedInUserFBImg"></div>
+<!--                    <div id="loggedInUserFBImg"></div>-->
                 </div>
             </nav>
 
