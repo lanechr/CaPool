a5eb0399e773169e3fc59c3bcf31ffed0f784faa
lanechr <lanechr@hotmail.com>
2016-05-16 18:18:07 +1000

    Removed map controls
    Got rid of street view and the satellite toggle

--- js/map.js
+++ js/map.js
@@ -42,6 +42,8 @@ $(document).ready(function initMap(){
     var directionsDisplay = new google.maps.DirectionsRenderer;
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
+        streetViewControl: false,
+        mapTypeControl: false,
         center: {lat: -27.495421, lng: 153.012470}
     });
     
--- js/mapTwo.js
+++ js/mapTwo.js
@@ -31,6 +31,8 @@ $(document).ready(initMap);
     var directionsDisplay = new google.maps.DirectionsRenderer;
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
+        streetViewControl: false,
+        mapTypeControl: false,
         center: {lat: -27.495421, lng: 153.012470}
     });
         
