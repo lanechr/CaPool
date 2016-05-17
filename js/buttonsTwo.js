1b7fc92e35ee83ab7e9c116aa5b9ddbcb927c5df
lanechr <lanechr@hotmail.com>
2016-05-17 14:50:52 +1000

    Facebook image att 2

--- js/buttonsTwo.js
+++ js/buttonsTwo.js
@@ -5,8 +5,7 @@
 var token;
 var fbLoggedIn;
 
-function driverList()
-{
+function driverList() {
     var visiblePassenger = $('#drivers .driverList:visible');
     visiblePassenger.hide();
     var nextToShow = $(visiblePassenger).next('.driverList:hidden');
@@ -18,7 +17,7 @@ function driverList()
     return false;
 };
 
-$(document).ready(function(){
+$(document).ready(function () {
     // Left Panel (HIDE)
     $(".passengerInfo").hide();
     // Tutorial (HIDE)
@@ -31,43 +30,43 @@ $(document).ready(function(){
     $("#pac-input").hide();
     // Options Menu (HIDE)
     $(".optionsMenu").hide();
-    
+
     $("#hiddenfacebookloginform").hide();
     $("#status").hide();
-    
+
     //Hide signup
-//    $("#signupOverlay").hide();
-//    $("#signupBlur").hide();
-    
+    //    $("#signupOverlay").hide();
+    //    $("#signupBlur").hide();
+
     // Opening Overaly (SHOW)
-//    $("#openingOverlay").hide();
-//    $("#openingBlur").hide();
-//    
-//    $('#loginOverlay').hide();
-//    $('#loginBlur').hide();
+    //    $("#openingOverlay").hide();
+    //    $("#openingBlur").hide();
+    //    
+    //    $('#loginOverlay').hide();
+    //    $('#loginBlur').hide();
     slider();
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     $("#datepicker").hide();
-    $('#cmn-toggle-4').change(function(){
-        if(this.checked)
+    $('#cmn-toggle-4').change(function () {
+        if (this.checked)
             $('#datepicker').fadeOut();
         else
             $('#datepicker').fadeIn();
     });
 });
 
-$(document).ready(function(){
-    $('#cmn-toggle-1').change(function(){
-        if(this.checked)
+$(document).ready(function () {
+    $('#cmn-toggle-1').change(function () {
+        if (this.checked)
             $('#end').fadeOut();
         else
             $('#end').fadeIn();
     });
 });
 
-function optionsMenu(){
+function optionsMenu() {
     var optionsMenu = $(".optionsMenu");
     if ($(".optionsMenu").is(":visible"))
         $(".optionsMenu").hide();
@@ -76,8 +75,7 @@ function optionsMenu(){
 }
 
 // Initialises Tutorial
-function passengerMode()
-{
+function passengerMode() {
     // Tutorial (SHOW)
     $("#tutorial").show();
     $("#tutorialBlur").show();
@@ -87,8 +85,7 @@ function passengerMode()
     $("#openingBlur").hide();
 }
 
-function hideTutorial()
-{
+function hideTutorial() {
     // Tutorial (HIDE)
     $("#tutorial").hide();
     $("#tutorialBlur").hide();
@@ -107,28 +104,27 @@ function datePicker() {
 
 // Used help from the below site for the implementation of slide-in animations
 // http://stackoverflow.com/questions/16989585/css-slide-in-from-left-transition
-function slider()
-{
-var $drivers = document.getElementById('drivers');
-var $toggle = document.getElementById('toggle');
+function slider() {
+    var $drivers = document.getElementById('drivers');
+    var $toggle = document.getElementById('toggle');
 
-$toggle.addEventListener('click', function() {
-    var isOpen = drivers.classList.contains('slide-in');
+    $toggle.addEventListener('click', function () {
+        var isOpen = drivers.classList.contains('slide-in');
 
-    drivers.setAttribute('class', isOpen ? 'passengerInfo container whiteBG slide-out' : 'passengerInfo container whiteBG slide-in');
-});
+        drivers.setAttribute('class', isOpen ? 'passengerInfo container whiteBG slide-out' : 'passengerInfo container whiteBG slide-in');
+    });
 }
 
 function loggedIn() {
 
     $('#loginOverlay').hide();
     $('#loginBlur').hide();
-    
+
     $("#openingOverlay").show();
     $("#openingBlur").show();
-    
+
     $("#logoutform").show();
-    
+
     $("#signupOverlay").hide();
     $("#signupBlur").hide();
 }
@@ -137,12 +133,12 @@ function notLoggedIn() {
 
     $('#loginOverlay').show();
     $('#loginBlur').show();
-    
+
     $("#openingOverlay").hide();
     $("#openingBlur").hide();
-    
+
     $("#logoutform").hide();
-    
+
     $("#signupOverlay").hide();
     $("#signupBlur").hide();
 }
@@ -151,12 +147,12 @@ function showSignup() {
 
     $('#loginOverlay').hide();
     $('#loginBlur').hide();
-    
+
     $("#openingOverlay").hide();
     $("#openingBlur").hide();
-    
+
     $("#logoutform").hide();
-    
+
     $("#signupOverlay").show();
     $("#signupBlur").show();
 }
@@ -165,12 +161,12 @@ function showLogin() {
 
     $('#loginOverlay').show();
     $('#loginBlur').show();
-    
+
     $("#openingOverlay").hide();
     $("#openingBlur").hide();
-    
+
     $("#logoutform").hide();
-    
+
     $("#signupOverlay").hide();
     $("#signupBlur").hide();
 }
@@ -178,80 +174,90 @@ function showLogin() {
 
 
 // initialise and setup facebook js sdk
-window.fbAsyncInit = function() { 
-        FB.init({
-          appId      : '1091776767535329',
-          xfbml      : true,
-          version    : 'v2.6'
-        });
-        FB.getLoginStatus(function(response) {
-            if (response.status === 'connected') {
-                document.getElementById('status').innerHTML = 'You are connected.';
-                $.ajax({
-                      type: "POST",
-                      url: "somescript.php",
-                      datatype: "html",
-                      data: dataString,
-                      success: function(data) {
-                          var fbLoginStatus = data;
-                          if (fbLoginStatus == "true"){
-                              insertUserProfilePic();
-                          }
-                        }
-                    });
-            } else if (response.status === 'not_authorized') {
-                document.getElementById('status').innerHTML = 'We are not logged in.'
-            } else {
-                document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
-            }
-        });
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '1091776767535329'
+        , xfbml: true
+        , version: 'v2.6'
+    });
+    FB.getLoginStatus(function (response) {
+        if (response.status === 'connected') {
+            document.getElementById('status').innerHTML = 'You are connected.';
+            $.ajax({
+                type: "POST"
+                , url: "checkfblogin.php"
+                , success: function (data) {
+                    var fbLoginStatus = data;
+                    if (fbLoginStatus == "true") {
+                        insertUserProfilePic();
+                    }
+                }
+            });
+        } else if (response.status === 'not_authorized') {
+            document.getElementById('status').innerHTML = 'We are not logged in.'
+        } else {
+            document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
+        }
+    });
 };
 
-(function(d, s, id){
+(function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
     js.src = "//connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+}(document, 'script', 'facebook-jssdk'));
 
-    function login () { 
-        FB.login(function(response) {
-            token = response.authResponse.accessToken;
-            if (response.status === 'connected') {
-                document.getElementById('status').innerHTML = 'You are connected.';
-                document.getElementById('login').style.visibility = 'hidden';
-                
-                FB.api('/me', 'GET', {access_token: token, fields: 'first_name, last_name, id, email'}, function(response) {
-                    //login though facebook
-                    $("#facebookidinput").val(response.id);
-                    $("#facebookfnameinput").val(response.first_name);
-                    $("#facebooklnameinput").val(response.last_name);
-                    $("#facebookemailinput").val(response.email);
-                    $("#hiddenfacebookloginform").submit();
-                    });
-            } else if (response.status === 'not_authorized') {
-                document.getElementById('status').innerHTML = 'You are not logged in.'
-            } else {
-                document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
-            }
-        }, {scope: 'public_profile,email' });
-        
-    }
+function login() {
+    FB.login(function (response) {
+        token = response.authResponse.accessToken;
+        if (response.status === 'connected') {
+            document.getElementById('status').innerHTML = 'You are connected.';
+            document.getElementById('login').style.visibility = 'hidden';
 
-    function getInfo() {
-        FB.api('/me', 'GET', {fields: 'first_name, last_name, name, id'}, function(response) {
-            document.getElementById('status').innerHTML = response.id + " you are logged in";
-        });
+            FB.api('/me', 'GET', {
+                access_token: token
+                , fields: 'first_name, last_name, id, email'
+            }, function (response) {
+                //login though facebook
+                $("#facebookidinput").val(response.id);
+                $("#facebookfnameinput").val(response.first_name);
+                $("#facebooklnameinput").val(response.last_name);
+                $("#facebookemailinput").val(response.email);
+                $("#hiddenfacebookloginform").submit();
+            });
+        } else if (response.status === 'not_authorized') {
+            document.getElementById('status').innerHTML = 'You are not logged in.'
+        } else {
+            document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
+        }
+    }, {
+        scope: 'public_profile,email'
+    });
 
-    }
+}
 
-    function insertUserProfilePic(){
-        FB.api('/me', 'GET', {fields: 'id'}, function(response) {
-            $('#loggedInUserFBImg').css("background-image", "url(https://graph.facebook.com/"+response.id+"/picture?type=large)");
-        });
-    }
+function getInfo() {
+    FB.api('/me', 'GET', {
+        fields: 'first_name, last_name, name, id'
+    }, function (response) {
+        document.getElementById('status').innerHTML = response.id + " you are logged in";
+    });
 
-function insertProfilePicByID(FBID){
-        $('#loggedInUserFBImg').css("background-image", "url(https://graph.facebook.com/"+FBID+"/picture?type=large)");
-    }
+}
+
+function insertUserProfilePic() {
+    FB.api('/me', 'GET', {
+        fields: 'id'
+    }, function (response) {
+        $('#loggedInUserFBImg').css("background-image", "url(https://graph.facebook.com/" + response.id + "/picture?type=large)");
+    });
+}
+
+function insertProfilePicByID(FBID) {
+    $('#loggedInUserFBImg').css("background-image", "url(https://graph.facebook.com/" + FBID + "/picture?type=large)");
+}
