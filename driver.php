af7c43c4b7da44a97ef9dfa9c6f975f4592ca582
Mitchell Woods <themitchwoods@gmail.com>
2016-05-17 21:30:04 +1000

    Destination Input Styling
    Adjusted the destination input width, and resulting menu location.

--- driver.php
+++ driver.php
@@ -112,7 +112,7 @@
 					<h2 class="ratingText blackBG">2</h2>
 				</div>
 
-				<div class="ratings">
+				<div class="ratings ratingsSecond">
 					<img src="images/money.png" alt="Payment" class="ratingIcon">
 					<h2 class="ratingText greenBG">Parking</h2>
 				</div>
