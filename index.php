32e5deb25518aa40770164d00f2e24dcfaf4963b
Mitchell Woods <themitchwoods@gmail.com>
2016-05-17 20:22:22 +1000

    Testing Viewport

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ session_start();
         <meta charset="utf-8" />
         <title>CaPool</title>
         <!-- Mobile Optimisation -->
-        <meta name=viewport content="width=device-width, initial-scale=1">
+        <meta name=viewport content="width=device-width, initial-scale=1, min-scale=1, max-scale=1, shrink-to-fit=no">
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
         <!-- StyleSheets -->
