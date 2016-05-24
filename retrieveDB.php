31008e8df86ddcbb1dde2a8f16b37bc01a195ba2
Freya Rogers <rogersfreya@gmail.com>
2016-05-25 09:09:38 +1000

    Adjustment of variables
    Adjusted variables. Still may need minor adjustments

--- retrieveDB.php
+++ retrieveDB.php
@@ -9,43 +9,44 @@ if (!$con)
   die('Could not connect: ' . mysql_error());
 }
  
-mysql_select_db("inmoti6_mysite", $con);
+mysql_select_db("dbuser", $con);
+
 
 $article_id = $_GET['id'];
 
 if( ! is_numeric($article_id) )
   die('invalid article id');
 
-$query = "SELECT * FROM `comments` WHERE `articleid` =$article_id LIMIT 0 , 30";
+$query = "SELECT * FROM users";
 
 $comments = mysql_query($query);
 
 while($row = mysql_fetch_array($comments, MYSQL_ASSOC))
 {
-	$name = $row['name'];
-	$rating = $row['rating'];
-	$seats = $row['seats'];
-	$payment = $row['payment'];
-	$startLat = $row['startLat'];
-	$endLat = $row['endLat'];
+	$id = $row['id'];
+  $email = $row['email'];
+  $password = $row['password'];
+  $facebookid = $row['facebookid'];
+  $fname = $row['fname'];
+  $lname = $row['lname'];
+  $rating = $row['rating'];
+  $responses = $row['responses'];
 	
   
-  	$name = htmlspecialchars($row['name'],ENT_QUOTES);
-  	$rating = htmlspecialchars($row['rating'],ENT_QUOTES);
-  	$seats = htmlspecialchars($row['seats'],ENT_QUOTES);
-  	$payment = htmlspecialchars($row['payment'],ENT_QUOTES);
-	$startLat = htmlspecialchars($row['startLat'],ENT_QUOTES);
-	$endLat = htmlspecialchars($row['endLat'],ENT_QUOTES);
+  $name = htmlspecialchars($row['name'],ENT_QUOTES);
+  $email = htmlspecialchars($row['email'],ENT_QUOTES);
+  $password = htmlspecialchars($row['password'],ENT_QUOTES);
+  $facebookid = htmlspecialchars($row['facebookid'],ENT_QUOTES);
+  $fname = htmlspecialchars($row['fname'],ENT_QUOTES);
+  $lname = htmlspecialchars($row['lname'],ENT_QUOTES);
+  $rating = htmlspecialchars($row['rating'],ENT_QUOTES);
+  $responses = htmlspecialchars($row['responses'],ENT_QUOTES);
   
-  echo "  <div style='margin:30px 0px;'>
-      Name: $name<br />
-      Email: $email<br />
-      Website: $website<br />
-      Comment: $comment<br />
-      Timestamp: $timestamp
-    </div>
-  ";
 }
+
+
+
+
 
 mysql_close($con);
 
