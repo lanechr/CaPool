1ab6a40f991f287f6637dc10921d9ad821ba3ee4
Mitchell Woods <themitchwoods@gmail.com>
2016-05-28 15:22:11 +1000

    DB Display

--- index.php
+++ index.php
@@ -305,7 +305,7 @@ session_start();
 				// Loop the recordset $results
 				// Each row will be made into an array ($row) using mysql_fetch_array
 				while($row = mysql_fetch_array($results)){
-					echo "<h1 class=marginPix>"$row['fname'];"</h1>"
+					echo "<h1 class=marginPix>"$row['fname']"</h1>";
 
 					// Close the database connection
 					mysql_close();
